851bea77ac5ad350267b7c2a128b350922c52f0a
Miguel Fonseca <fmmfonseca@gmail.com>
2010-04-03 11:48:25 +0100

    unit tests enhanced

--- test/tc_data_table.rb
+++ test/tc_data_table.rb
@@ -67,16 +67,16 @@ class TC_DataTable < Test::Unit::TestCase
   end
   
   def test_close
-    columns = [DataType::STRING, DataColumn.new(DataType::NUMBER)]
-    rows = [['value', 0], DataRow.new(['value', 1])]
+    columns = [DataType::STRING, DataType::NUMBER]
+    rows = [['value', 0], ['value', 1]]
     table = DataTable.new(columns, rows)
     assert_raise(StandardError) { table.add_column(DataColumn.new(DataType::BOOLEAN)) }
     assert_nothing_raised { table.add_row(['value', 2]) }
   end
   
   def test_sort_rows_single_column_ascending
-    columns = [DataType::STRING, DataColumn.new(DataType::NUMBER)]
-    rows = [['value', 2], ['value', 0], DataRow.new(['value', 1])]
+    columns = [DataType::STRING, DataType::NUMBER]
+    rows = [['value', 2], ['value', 0], ['value', 1]]
     table = DataTable.new(columns, rows)
     table.sort_rows!(:ascending, 1)
     assert_equal(table.cell(0,1).value, 0)
@@ -85,8 +85,8 @@ class TC_DataTable < Test::Unit::TestCase
   end
   
   def test_sort_rows_single_column_descending
-    columns = [DataType::STRING, DataColumn.new(DataType::NUMBER)]
-    rows = [['value', 2], ['value', 0], DataRow.new(['value', 1])]
+    columns = [DataType::STRING, DataType::NUMBER]
+    rows = [['value', 2], ['value', 0], ['value', 1]]
     table = DataTable.new(columns, rows)
     table.sort_rows!(:descending, 1)
     assert_equal(table.cell(0,1).value, 2)
@@ -95,23 +95,25 @@ class TC_DataTable < Test::Unit::TestCase
   end
  
   def test_sort_rows_multiple_columns_ascending
-    columns = [DataType::STRING, DataColumn.new(DataType::NUMBER)]
-    rows = [['b', 1], ['a', 1], DataRow.new(['c', 0])]
+    columns = [DataType::STRING, DataType::NUMBER, DataType::NUMBER]
+    rows = [['d', 2, 0], ['b', 1, 1], ['a', 1, 2], ['c', 0, 3]]
     table = DataTable.new(columns, rows)
-    table.sort_rows!(:ascending, 1, 0)
+    table.sort_rows!(:ascending, 1, 2)
     assert_equal(table.cell(0,0).value, 'c')
-    assert_equal(table.cell(1,0).value, 'a')
-    assert_equal(table.cell(2,0).value, 'b')
+    assert_equal(table.cell(1,0).value, 'b')
+    assert_equal(table.cell(2,0).value, 'a')
+    assert_equal(table.cell(3,0).value, 'd')
   end
   
   def test_sort_rows_multiple_columns_descending
-    columns = [DataType::STRING, DataColumn.new(DataType::NUMBER)]
-    rows = [['b', 1], ['a', 1], DataRow.new(['c', 0])]
+    columns = [DataType::STRING, DataType::NUMBER, DataType::NUMBER]
+    rows = [['d', 2, 0], ['b', 1, 1], ['a', 1, 2], ['c', 0, 3]]
     table = DataTable.new(columns, rows)
-    table.sort_rows!(:descending, 1, 0)
-    assert_equal(table.cell(0,0).value, 'b')
+    table.sort_rows!(:descending, 1, 2)
+    assert_equal(table.cell(0,0).value, 'd')
     assert_equal(table.cell(1,0).value, 'a')
-    assert_equal(table.cell(2,0).value, 'c')
+    assert_equal(table.cell(2,0).value, 'b')
+    assert_equal(table.cell(3,0).value, 'c')
   end
 
   def test_custom_properties
