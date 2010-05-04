e38b6eac89c3a94039f65c5f9118df0a3a292f2c
Ben Tucker <ben@btucker.net>
2010-05-04 13:20:33 -0400

    Start using json gem for json generation to ensure validity

--- lib/google_visualization/formatter/json.rb
+++ lib/google_visualization/formatter/json.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 module Google
   module Visualization
     module Formatter
@@ -17,13 +19,11 @@ module Google
         # Generates a JSON string representation of a data table.
         #
         def self.render(data_table)
-          result = []
-          result << render_columns(data_table.columns)
-          result << render_rows(data_table.rows, data_table.columns)
-          if data_table.custom_properties_count > 0
-            result << render_custom_properties(data_table)
-          end
-          "{#{result.join(',')}}"
+          {
+            'cols' => render_columns(data_table),
+            'rows' => render_rows(data_table) #,
+#            'p' => render_custom_properties(data_table)
+          }.to_json
         end
 
         private
@@ -31,171 +31,59 @@ module Google
         ##
         # Generates a JSON string representation of a multiple columns.
         #
-        def self.render_columns(columns)
-          result = columns.collect { |column|
-            render_column(column)
-          }
-          "cols:[#{result.join(',')}]"
+        def self.render_columns(data_table)
+          data_table.columns.map{|c| render_column(c)}
         end
 
         ##
         # Generates a JSON string representation of a single column.
         #
         def self.render_column(column)
-          result = []
-          unless column.id.nil?
-            result << "id:#{rb_to_js(column.id, DataType::STRING)}"
+          result = {}
+          %(id label type pattern).each do |field|
+            result[field] = column.send(field).to_s if column.send(field)
           end
-          unless column.label.nil?
-            result << "label:#{rb_to_js(column.label, DataType::STRING)}"
-          end
-          result << "type:#{rb_to_js(column.type.to_s, DataType::STRING)}"
-          unless column.pattern.empty?
-            result << "pattern:#{rb_to_js(column.pattern, DataType::STRING)}"
-          end
-          if column.custom_properties_count > 0
-            result << render_custom_properties(column)
-          end
-          "{#{result.join(',')}}"
+         # if column.custom_properties_count > 0
+         #   result["p"] = render_custom_properties(column)
+         # end
+          result
         end
 
         ##
         # Generates a JSON string representation of multiple rows.
         #
-        def self.render_rows(rows, columns)
-          result = rows.collect { |row|
-            render_row(row, columns)
-          }
-          "rows:[#{result.join(',')}]"
+        def self.render_rows(data_table)
+          data_table.rows.map{|r| render_row(r, data_table)}
         end
 
         ##
         # Generates a JSON string representation of a single row.
         #
-        def self.render_row(row, columns)
-          result = []
-          cells = columns.zip(row.cells).collect { |column, cell|
-            render_cell(cell, column.type)
-          }
-          result << "c:[#{cells.join(',')}]"
-          if row.custom_properties_count > 0
-            result << render_custom_properties(row)
-          end
-          "{#{result.join(',')}}"
+        def self.render_row(row, data_table)
+          result = {'c' => []}
+          result = {'c' => row.cells.map {|c| render_cell(c)}}
+          result
         end
 
         ##
         # Generates a JSON string representation of a cell.
         #
-        def self.render_cell(cell, data_type)
-          result = []
-          result << "v:#{rb_to_js(cell.value, data_type)}"
-          unless cell.formatted_value.nil?
-            result << "f:#{rb_to_js(cell.formatted_value, DataType::STRING)}"
-          end
-          if cell.custom_properties_count > 0
-            result << render_custom_properties(cell)
-          end
-          "{#{result.join(',')}}"
+        def self.render_cell(cell)
+          result = {'v' => cell.value} 
+          result['f'] = cell.formatted_value if cell.formatted_value
+          # result['p'] = render_custom_properties(row) if row.custom_properties_count > 0
         end
 
         ##
         # Generates a JSON string representation of the custom properties.
         #
-        def self.render_custom_properties(data_element)
-          result = data_element.custom_properties.collect { |name,value|
-            "#{name.to_s}:#{rb_to_js(value, DataType::STRING)}"
-          }
-          "p:{#{result.join(',')}}"
-        end
-
-        ##
-        # Converts a Ruby value into a JavaScript value.
-        #
-        def self.rb_to_js(obj, type)
-          return "null" if obj.nil?
-
-          case(type)
-          when DataType::STRING
-            return rb_to_js_string(obj)
-          when DataType::NUMBER
-            return rb_to_js_number(obj)
-          when DataType::BOOLEAN
-            return rb_to_js_boolean(obj)
-          when DataType::DATE
-            return rb_to_js_date(obj)
-          when DataType::DATETIME
-            return rb_to_js_datetime(obj)
-          when DataType::TIMEOFDAY
-            return rb_to_js_timeofday(obj)
-          else
-            raise(TypeError, "wrong argument type #{type}, should be DataType")
-          end
-        end
-
-        ##
-        # Converts a Ruby value into a JavaScript string.
-        #
-        def self.rb_to_js_string(obj)
-          obj.to_s.inspect
-        end
-
-        ##
-        # Converts a Ruby value into a JavaScript number.
-        #
-        def self.rb_to_js_number(obj)
-          if obj.kind_of?(Integer) or obj.instance_of?(Float)
-            obj.to_s
-          elsif obj.respond_to?(:to_i)
-            obj.to_i.to_s
-          else
-            raise(TypeError, "can't convert #{obj.class} into JavaScript number")
-          end
-        end
-
-        ##
-        # Converts a Ruby value into a JavaScript boolean.
-        #
-        def self.rb_to_js_boolean(obj)
-          (obj == false or obj.nil?) ? false.to_s : true.to_s
-        end
-
-        ##
-        # Converts a Ruby value into a JavaScript date.
-        #
-        def self.rb_to_js_date(obj)
-          if obj.instance_of?(Date) or obj.instance_of?(Time) or obj.instance_of?(DateTime)
-            #Shift the month by 1 to match the JavaScript date format
-            "new Date(#{obj.year},#{obj.month-1},#{obj.day})"
-          else
-            raise(TypeError, "can't convert #{obj.class} into JavaScript date")
-          end
-        end
-
-        ##
-        # Converts a Ruby value into a JavaScript datetime.
-        #
-        def self.rb_to_js_datetime(obj)
-          if obj.instance_of?(Time) or obj.instance_of?(DateTime)
-            #Shift the month by 1 to match the JavaScript date format
-            "new Date(#{obj.year},#{obj.month-1},#{obj.day},#{obj.hour},#{obj.min},#{obj.sec})"
-          else
-            raise(TypeError, "can't convert #{obj.class} into JavaScript datetime")
-          end
-        end
-
-        ##
-        # Converts a Ruby value into a JavaScript timeofday.
-        #
-        def self.rb_to_js_timeofday(obj)
-          if obj.instance_of?(Time) or obj.instance_of?(DateTime)
-            "[#{obj.hour},#{obj.min},#{obj.sec}]"
-          else
-            raise(TypeError, "can't convert #{obj.class} into JavaScript timeofday")
-          end
-        end
+        #def self.render_custom_properties(data_element)
+        #  result = data_element.custom_properties.collect { |name,value|
+        #    "#{name.to_s}:#{rb_to_js(value, DataType::STRING)}"
+        #  }
+        #  "p:{#{result.join(',')}}"
+        #end
       end
-
     end
   end
 end
